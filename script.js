522acb7b90477fef923530d51565c9b0be3b5a16
LidiaS98 <107851587+LidiaS98@users.noreply.github.com>
2023-06-20 13:44:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const summaryItemsContainer = document.querySelector(".summaryItems");
+
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const dataScore = data
+      .map((item) => {
+        const { category, score, icon, alt, itemClass, textClass } = item;
+        return `
+        <div class="item ${itemClass}">
+            <img alt="${alt}" class="icon" src=${icon}>
+            <p class=${textClass}>${category}</p>
+            <p class="score-items">${score}<span>   / 100</span></p>
+      </div>`;
+      })
+      .join("");
+    console.log(dataScore);
+    summaryItemsContainer.innerHTML = dataScore;
+  })
+  .catch((error) => console.error(error));
